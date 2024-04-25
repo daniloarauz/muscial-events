a4997b3b11e307f950d70ece818cbf91ef55158d
Oscar Arauz <daniloarauz@gmail.com>
2024-04-25 05:58:53 -0400

    Update Home Component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../shared/components/header/header.component';
 import { FooterComponent } from '../shared/components/footer/footer.component';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, MatSelectModule, MatFormFieldModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
