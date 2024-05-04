06dcbff8a0e92c4e604c6a24ec086b6a5be9413c
Oscar Arauz <daniloarauz@gmail.com>
2024-05-04 13:43:49 -0400

    Create Text Limit pipe

---
+++ src/app/shared/pipes/text-limit.pipe.ts
@@ -0,0 +1,18 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'textLimit',
+  standalone: true
+})
+export class TextLimitPipe implements PipeTransform {
+
+  limit = 20;
+
+  transform(value: string, ...args: unknown[]): string {
+    if(value.length > this.limit){
+      return value.substring(0, this.limit) + "..."
+    }
+    return value;
+  }
+
+}
