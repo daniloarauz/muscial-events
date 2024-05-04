9dc5f153c85c6d7d521f27654e39030f910febd5
Oscar Arauz <daniloarauz@gmail.com>
2024-05-04 13:41:36 -0400

    Update Register Component to use Simple Header instead of Header

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from '../shared/components/header/header.component';
 import { FooterComponent } from '../shared/components/footer/footer.component';
+import { SimpleHeaderComponent } from '../shared/components/simple-header/simple-header.component';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent],
+  imports: [SimpleHeaderComponent, FooterComponent],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
