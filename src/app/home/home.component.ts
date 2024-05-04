195e2206355583277f7c57f0afabb9612913121f
Oscar Arauz <daniloarauz@gmail.com>
2024-05-04 13:32:53 -0400

    Implement Genre filtering in Home Component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, inject } from '@angular/core';
-import { HeaderComponent } from '../shared/components/header/header.component';
+import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from '../shared/components/footer/footer.component';
 import { MatSelectModule } from '@angular/material/select';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -8,25 +8,53 @@ import { HomeService } from './home.service';
 import { Genre } from '../shared/models/genre.model';
 import { NgFor } from '@angular/common';
 import { Concert } from '../shared/models/concert.model';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent, MatSelectModule, MatFormFieldModule, EventCardComponent, NgFor],
+  imports: [
+    HeaderComponent,
+    FooterComponent,
+    MatSelectModule,
+    MatFormFieldModule,
+    EventCardComponent,
+    NgFor,
+    ReactiveFormsModule,
+  ],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent implements OnInit {
   genres: Genre[] = [];
-  concerts: Concert[] = [];
+  initialConcerts: Concert[] = [];
+  currentConcerts: Concert[] = [];
 
   homeService = inject(HomeService);
 
-   ngOnInit(){
-    this.homeService.getData().subscribe((data)=> {
+  currentGenre = new FormControl(0);
+
+  ngOnInit() {
+    this.homeService.getData().subscribe((data) => {
       console.log(data);
       this.genres = data.genres;
-      this.concerts = data.concerts;
+      this.initialConcerts = this.currentConcerts = data.concerts;
+    });
+
+    this.currentGenre.valueChanges.
+    subscribe((val:number | null) => {
+    /*  if (val === 0) {
+        this.currentConcerts = this.initialConcerts;
+      } else {
+        this.currentConcerts = this.initialConcerts.filter(
+          (item) => item.genreId === val
+        );
+    }*/
+
+      this.currentConcerts =
+      val === 0
+      ? this.initialConcerts
+      : this.initialConcerts.filter((item) => item.genreId === val);
     });
   }
 }
