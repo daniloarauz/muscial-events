51df85dfe3048202b8f65abbec2ccfe66fadb440
Oscar Arauz <daniloarauz@gmail.com>
2024-05-04 13:35:26 -0400

    Implement text-limit pipe in Event Card

--- src/app/home/event-card/event-card.component.ts
+++ src/app/home/event-card/event-card.component.ts
@@ -1,10 +1,12 @@
 import { Component, Input, OnInit, input } from '@angular/core';
 import { Concert } from '../../shared/models/concert.model';
+import { TextLimitPipe } from '../../shared/pipes/text-limit.pipe';
+import { UpperCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-event-card',
   standalone: true,
-  imports: [],
+  imports: [TextLimitPipe, UpperCasePipe],
   templateUrl: './event-card.component.html',
   styleUrl: './event-card.component.css'
 })
